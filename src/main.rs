727c6a034afc94ba08ad480809259e0189ea4c55
pembem22 <24252798+pembem22@users.noreply.github.com>
2024-02-01 13:48:33 +0200

    Shows one camera

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 use std::{
+    cmp::min,
     collections::HashMap,
-    env, str,
-    sync::Arc,
-    sync::Mutex as StdMutex,
+    env,
+    io::Cursor,
+    str,
+    sync::{Arc, Mutex as StdMutex},
     time::{Duration, SystemTime},
 };
 
-use tokio::{
-    io::{split, AsyncReadExt, AsyncWriteExt},
-    net::UdpSocket,
-    sync::Mutex,
-    task::JoinHandle,
-};
-use tokio_serial::SerialPortBuilderExt;
+use imgui::TextureId;
+use imgui_wgpu::{Texture, TextureConfig};
+use tokio::{io::AsyncReadExt, join, sync::Mutex, task::JoinHandle};
+use tokio_serial::{SerialPort, SerialPortBuilderExt};
 
 use hex_literal::hex;
+use wgpu::Extent3d;
+
+use image::{codecs::jpeg::JpegDecoder, ImageDecoder};
 
 #[cfg(unix)]
 const DEFAULT_TTY: &str = "/dev/ttyUSB0";
@@ -25,46 +27,49 @@ const BAUD_RATE: u32 = 3000000;
 
 const ETVR_PACKET_HEADER: [u8; 4] = hex!("FF A0 FF A1");
 
+const CAMERA_FRAME_SIZE: u32 = 240;
+
+struct Frame {
+    data: Vec<u8>,
+    timestamp: SystemTime,
+}
+
 struct Camera {
     eye: Eye,
-    frame: Arc<Mutex<Vec<u8>>>,
-    frame_time: SystemTime,
+    frame: Arc<Mutex<Frame>>,
     task: Option<JoinHandle<()>>,
 }
 
 impl Camera {
     fn new(eye: Eye) -> Camera {
-        let frame: Arc<Mutex<Vec<u8>>> = Arc::new(Mutex::new(Vec::new()));
-
         Camera {
             eye,
-            frame,
-            frame_time: SystemTime::now(),
+            frame: Arc::new(Mutex::new(Frame {
+                data: Vec::new(),
+                timestamp: SystemTime::now(),
+            })),
             task: None,
         }
     }
 
-    fn start(mut self, tty_path: String) -> tokio_serial::Result<()> {
+    fn start(&mut self, tty_path: String) -> tokio_serial::Result<()> {
+        let frame = self.frame.clone();
+        let eye = self.eye;
+
         let mut port = tokio_serial::new(tty_path, BAUD_RATE).open_native_async()?;
 
         let future = async move {
-            let mut vec = Vec::new();
+            let mut remaining_bytes = Vec::new();
             'find_packet: loop {
-                let mut buf = vec![0; 2048];
-                port.read_exact(&mut buf).await.unwrap();
+                remaining_bytes.resize(remaining_bytes.len() + 2048, 0);
+                let read_position = remaining_bytes.len() - 2048;
+                port.read_exact(&mut remaining_bytes[read_position..])
+                    .await
+                    .unwrap();
 
-                vec.append(&mut buf);
-                drop(buf);
-
-                for i in 0..vec.len() - ETVR_PACKET_HEADER.len() - 2 + 1 {
-                    if vec[i..i + ETVR_PACKET_HEADER.len()] == ETVR_PACKET_HEADER {
-                        let len = u16::from_le_bytes([vec[i + 4], vec[i + 5]]);
-                        vec.drain(0..i + 6);
-                        vec.resize(len as usize, 0);
-                        port.read_exact(&mut vec).await.unwrap();
-                        let mut frame_vec = self.frame.lock().await;
-                        self.frame_time = SystemTime::now();
-                        *frame_vec = vec;
+                for i in 0..remaining_bytes.len() - ETVR_PACKET_HEADER.len() - 2 + 1 {
+                    if remaining_bytes[i..i + ETVR_PACKET_HEADER.len()] == ETVR_PACKET_HEADER {
+                        remaining_bytes.drain(0..i);
                         break 'find_packet;
                     }
                 }
@@ -72,14 +77,38 @@ impl Camera {
 
             loop {
                 let mut buf = [0u8; 6];
-                port.read_exact(&mut buf).await.unwrap();
-                let len = u16::from_le_bytes([buf[4], buf[5]]);
 
-                let mut vec = vec![0; len as usize];
-                port.read_exact(&mut vec).await.unwrap();
-                self.frame_time = SystemTime::now();
-                let mut frame_vec = self.frame.lock().await;
-                *frame_vec = vec;
+                if !remaining_bytes.is_empty() {
+                    let to_copy = min(remaining_bytes.len(), 6);
+                    buf[..to_copy].copy_from_slice(&remaining_bytes[..to_copy]);
+                    remaining_bytes.drain(0..to_copy);
+                    port.read_exact(&mut buf[to_copy..]).await.unwrap();
+                } else {
+                    port.read_exact(&mut buf).await.unwrap();
+                }
+
+                assert_eq!(buf[0..4], ETVR_PACKET_HEADER);
+                let packet_len = u16::from_le_bytes([buf[4], buf[5]]) as usize;
+
+                let mut buf = vec![0; packet_len];
+
+                if !remaining_bytes.is_empty() {
+                    let to_copy = min(remaining_bytes.len(), packet_len);
+                    buf[..to_copy].copy_from_slice(&remaining_bytes[..to_copy]);
+                    remaining_bytes.drain(0..to_copy);
+                    port.read_exact(&mut buf[to_copy..]).await.unwrap();
+                } else {
+                    port.read_exact(&mut buf).await.unwrap();
+                }
+
+                let new_frame = Frame {
+                    timestamp: SystemTime::now(),
+                    data: buf,
+                };
+
+                *frame.lock().await = new_frame;
+
+                // println!("{:?} frame! {}", eye, port.bytes_to_read().unwrap());
             }
         };
 
@@ -89,18 +118,61 @@ impl Camera {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 enum Eye {
     L,
     R,
 }
 
+mod ui;
+
 #[tokio::main]
 async fn main() -> tokio_serial::Result<()> {
-    let l_camera = Camera::new(Eye::L);
-    let r_camera = Camera::new(Eye::R);
+    let mut l_camera = Camera::new(Eye::L);
+    // let mut r_camera = Camera::new(Eye::R);
 
     l_camera.start("COM3".to_string())?;
-    r_camera.start("COM4".to_string())?;
+    // r_camera.start("COM4".to_string())?;
 
+    let ui_task = tokio::task::spawn_blocking(|| {
+
+        let mut ui = ui::UI::new();
+
+        let texture_config: TextureConfig<'_> = TextureConfig {
+            size: wgpu::Extent3d {
+                width: CAMERA_FRAME_SIZE,
+                height: CAMERA_FRAME_SIZE,
+                ..Default::default()
+            },
+            label: Some("lenna texture"),
+            format: Some(wgpu::TextureFormat::Rgba8Unorm),
+            ..Default::default()
+        };
+
+        let texture = Texture::new(&ui.device, &ui.renderer, texture_config);
+        let l_texture_id = ui.renderer.textures.insert(texture);
+
+        ui.run(move |ui, queue, renderer| {
+            let jpeg_data = l_camera.frame.blocking_lock().data.clone();
+
+            let mut decoder = image::io::Reader::new(Cursor::new(jpeg_data));
+            decoder.set_format(image::ImageFormat::Jpeg);
+            let image = decoder.decode().unwrap();
+            // println!("{:?}", decoder.);
+
+            renderer.textures.get(l_texture_id).unwrap().write(
+                &queue,
+                &image.into_rgba8(),
+                CAMERA_FRAME_SIZE,
+                CAMERA_FRAME_SIZE,
+            );
+
+            ui.window("Hello!").build(|| {
+              imgui::Image::new(l_texture_id, [CAMERA_FRAME_SIZE as f32, CAMERA_FRAME_SIZE as f32]).build(ui)
+            });
+        });
+    });
+
+    join!(l_camera.task.unwrap(), ui_task);
     Ok(())
 }
---
+++ src/ui.rs
@@ -0,0 +1,280 @@
+use image::ImageFormat;
+use imgui::*;
+use imgui_wgpu::{Renderer, RendererConfig, Texture, TextureConfig};
+use pollster::block_on;
+use std::time::Instant;
+use wgpu::Extent3d;
+use winit::{
+    dpi::LogicalSize, event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent}, event_loop::{ControlFlow, EventLoop, EventLoopBuilder, EventLoopWindowTarget}, platform::windows::EventLoopBuilderExtWindows, window::Window
+};
+
+pub(crate) struct UI {
+    event_loop: winit::event_loop::EventLoop<()>,
+    surface: wgpu::Surface,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    window: winit::window::Window,
+    imgui: imgui::Context,
+    platform: imgui_winit_support::WinitPlatform,
+    pub renderer: imgui_wgpu::Renderer,
+}
+
+impl UI {
+    pub fn new() -> UI {
+        env_logger::init();
+
+        // Set up window and GPU
+        let event_loop = EventLoopBuilder::new().with_any_thread(true).build();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let (window, size, surface) = {
+            let version = env!("CARGO_PKG_VERSION");
+
+            let window = Window::new(&event_loop).unwrap();
+            window.set_inner_size(LogicalSize {
+                width: 1280.0,
+                height: 720.0,
+            });
+            window.set_title(&format!("imgui-wgpu {version}"));
+            let size = window.inner_size();
+
+            let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+            (window, size, surface)
+        };
+
+        let hidpi_factor = window.scale_factor();
+
+        let adapter = block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        }))
+        .unwrap();
+
+        let (mut device, mut queue) = block_on(adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+            },
+            None,
+        ))
+        .unwrap();
+
+        // Set up swap chain
+        let surface_desc = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            view_formats: vec![wgpu::TextureFormat::Bgra8Unorm],
+        };
+
+        surface.configure(&device, &surface_desc);
+
+        // Set up dear imgui
+        let mut imgui = imgui::Context::create();
+        let mut platform = imgui_winit_support::WinitPlatform::init(&mut imgui);
+        platform.attach_window(
+            imgui.io_mut(),
+            &window,
+            imgui_winit_support::HiDpiMode::Default,
+        );
+        imgui.set_ini_filename(None);
+
+        let font_size = (13.0 * hidpi_factor) as f32;
+        imgui.io_mut().font_global_scale = (1.0 / hidpi_factor) as f32;
+
+        imgui.fonts().add_font(&[FontSource::DefaultFontData {
+            config: Some(imgui::FontConfig {
+                oversample_h: 1,
+                pixel_snap_h: true,
+                size_pixels: font_size,
+                ..Default::default()
+            }),
+        }]);
+
+        //
+        // Set up dear imgui wgpu renderer
+        //
+        let renderer_config = RendererConfig {
+            texture_format: surface_desc.format,
+            ..Default::default()
+        };
+
+        let mut renderer = Renderer::new(&mut imgui, &device, &queue, renderer_config);
+
+        // Set up Lenna texture
+        // let lenna_bytes = include_bytes!("../resources/checker.png");
+        // let image =
+        //     image::load_from_memory_with_format(lenna_bytes, ImageFormat::Png).expect("invalid image");
+        // let image = image.to_rgba8();
+        // let (width, height) = image.dimensions();
+        // let raw_data = image.into_raw();
+
+        // let texture_config = TextureConfig {
+        //     size: Extent3d {
+        //         width,
+        //         height,
+        //         ..Default::default()
+        //     },
+        //     label: Some("lenna texture"),
+        //     format: Some(wgpu::TextureFormat::Rgba8Unorm),
+        //     ..Default::default()
+        // };
+
+        // let texture = Texture::new(&device, &renderer, texture_config);
+
+        // texture.write(&queue, &raw_data, width, height);
+        // let lenna_texture_id = renderer.textures.insert(texture);
+
+        // init(&device, &queue, &mut renderer);
+
+        UI {
+            event_loop,
+            surface,
+            device,
+            queue,
+            window,
+            imgui,
+            platform,
+            renderer,
+        }
+    }
+
+    pub fn run<F: Fn(&imgui::Ui, &wgpu::Queue, &mut imgui_wgpu::Renderer) + 'static>(mut self, render: F) {
+        let clear_color = wgpu::Color {
+            r: 0.1,
+            g: 0.2,
+            b: 0.3,
+            a: 1.0,
+        };
+
+        let mut last_frame = Instant::now();
+        let mut last_cursor = None;
+
+        self.event_loop.run(move |event, _, control_flow| {
+            *control_flow = if cfg!(feature = "metal-auto-capture") {
+                ControlFlow::Exit
+            } else {
+                ControlFlow::Poll
+            };
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::Resized(size),
+                    ..
+                } => {
+                    let surface_desc = wgpu::SurfaceConfiguration {
+                        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                        format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                        width: size.width,
+                        height: size.height,
+                        present_mode: wgpu::PresentMode::Fifo,
+                        alpha_mode: wgpu::CompositeAlphaMode::Auto,
+                        view_formats: vec![wgpu::TextureFormat::Bgra8Unorm],
+                    };
+
+                    self.surface.configure(&self.device, &surface_desc);
+                }
+                Event::WindowEvent {
+                    event:
+                        WindowEvent::KeyboardInput {
+                            input:
+                                KeyboardInput {
+                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    state: ElementState::Pressed,
+                                    ..
+                                },
+                            ..
+                        },
+                    ..
+                }
+                | Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                Event::MainEventsCleared => {
+                    self.window.request_redraw();
+                }
+                Event::RedrawEventsCleared => {
+                    let now = Instant::now();
+                    self.imgui.io_mut().update_delta_time(now - last_frame);
+                    last_frame = now;
+
+                    let frame = match self.surface.get_current_texture() {
+                        Ok(frame) => frame,
+                        Err(e) => {
+                            eprintln!("dropped frame: {e:?}");
+                            return;
+                        }
+                    };
+                    self.platform
+                        .prepare_frame(self.imgui.io_mut(), &self.window)
+                        .expect("Failed to prepare frame");
+                    let ui = self.imgui.frame();
+
+                    render(ui, &self.queue, &mut self.renderer);
+
+                    // {
+                    //     let size = [width as f32, height as f32];
+                    //     let window = ui.window("Hello world");
+                    //     window
+                    //         .size([400.0, 600.0], Condition::FirstUseEver)
+                    //         .build(|| {
+                    //             ui.text("Hello textures!");
+                    //             ui.text("Say hello to checker.png");
+                    //             Image::new(lenna_texture_id, size).build(ui);
+                    //         });
+                    // }
+
+                    let mut encoder: wgpu::CommandEncoder = self
+                        .device
+                        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+                    if last_cursor != Some(ui.mouse_cursor()) {
+                        last_cursor = Some(ui.mouse_cursor());
+                        self.platform.prepare_render(ui, &self.window);
+                    }
+
+                    let view = frame
+                        .texture
+                        .create_view(&wgpu::TextureViewDescriptor::default());
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: None,
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(clear_color),
+                                store: true,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                    });
+
+                    self.renderer
+                        .render(self.imgui.render(), &self.queue, &self.device, &mut rpass)
+                        .expect("Rendering failed");
+
+                    drop(rpass);
+
+                    self.queue.submit(Some(encoder.finish()));
+                    frame.present();
+                }
+                _ => (),
+            }
+
+            self.platform
+                .handle_event(self.imgui.io_mut(), &self.window, &event);
+        });
+    }
+}
