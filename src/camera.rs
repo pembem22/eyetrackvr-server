8f23a906a4725d9f14575464e351618305a8df81
pembem22 <24252798+pembem22@users.noreply.github.com>
2025-02-06 11:30:22 +0200

    Handle serial camera disconnection

--- src/camera.rs
+++ src/camera.rs
@@ -62,100 +62,114 @@ impl Camera {
         let future = async move {
             let mut reconnect = false;
 
-            if reconnect {
-                println!("Reconnecting in a sec to {tty_path}");
-                sleep(Duration::from_secs(1)).await;
-            }
-            reconnect = true;
+            'connect_loop: loop {
+                if reconnect {
+                    println!("Reconnecting in a sec to {tty_path}");
+                    sleep(Duration::from_secs(1)).await;
+                }
+                reconnect = true;
 
-            let mut port = 
-            match tokio_serial::new(tty_path.clone(), BAUD_RATE)
-                .open_native_async() {
-                    Ok(port) => port,
-                    Err(error) => {
-                        println!("Serial open error: {:?}", error.kind());
-                        return;
-                        // continue 'init;
-                    }
-                };
-            let mut remaining_bytes = Vec::new();
-            'init: loop {
-
-                'find_packet: loop {
-                    remaining_bytes.resize(remaining_bytes.len() + 2048, 0);
-                    let read_position = remaining_bytes.len() - 2048;
-                    
-                    match port.read_exact(&mut remaining_bytes[read_position..]).await {
-                        Ok(..) => (),
+                let mut port =
+                    match tokio_serial::new(tty_path.clone(), BAUD_RATE).open_native_async() {
+                        Ok(port) => port,
                         Err(error) => {
-                            println!("Serial read error: {}", error.kind());
+                            println!("Serial open error: {error:?}");
+                            // return;
+                            continue 'connect_loop;
+                        }
+                    };
+                let mut remaining_bytes = Vec::new();
+                'init: loop {
+                    'find_packet: loop {
+                        remaining_bytes.resize(remaining_bytes.len() + 2048, 0);
+                        let read_position = remaining_bytes.len() - 2048;
+
+                        match port.read_exact(&mut remaining_bytes[read_position..]).await {
+                            Ok(..) => (),
+                            Err(error) => {
+                                println!("Serial read error: {error:?}");
+                                // TODO: deduplicate
+                                if let Some(raw_err) = error.raw_os_error() {
+                                    if raw_err == 22 {
+                                        continue 'connect_loop;
+                                    }
+                                }
+                                continue 'init;
+                            }
+                        };
+
+                        for i in 0..remaining_bytes.len() - ETVR_PACKET_HEADER.len() - 2 + 1 {
+                            if remaining_bytes[i..i + ETVR_PACKET_HEADER.len()]
+                                == ETVR_PACKET_HEADER
+                            {
+                                remaining_bytes.drain(0..i);
+                                break 'find_packet;
+                            }
+                        }
+                    }
+
+                    loop {
+                        let mut buf = [0u8; 6];
+
+                        let to_copy = std::cmp::min(remaining_bytes.len(), 6);
+                        buf[..to_copy].copy_from_slice(&remaining_bytes[..to_copy]);
+                        remaining_bytes.drain(0..to_copy);
+                        match port.read_exact(&mut buf[to_copy..]).await {
+                            Ok(..) => (),
+                            Err(error) => {
+                                println!("Warning: failed to read exact frame: {error:?}");
+                                // continue 'init;
+                            }
+                        };
+
+                        if buf[0..4] != ETVR_PACKET_HEADER {
+                            println!("Wrong packet header");
                             continue 'init;
                         }
-                    };
+                        let packet_len = u16::from_le_bytes([buf[4], buf[5]]) as usize;
 
-                    for i in 0..remaining_bytes.len() - ETVR_PACKET_HEADER.len() - 2 + 1 {
-                        if remaining_bytes[i..i + ETVR_PACKET_HEADER.len()] == ETVR_PACKET_HEADER {
-                            remaining_bytes.drain(0..i);
-                            break 'find_packet;
+                        let mut buf = vec![0; packet_len];
+
+                        let to_copy = std::cmp::min(remaining_bytes.len(), packet_len);
+                        buf[..to_copy].copy_from_slice(&remaining_bytes[..to_copy]);
+                        remaining_bytes.drain(0..to_copy);
+                        match port.read_exact(&mut buf[to_copy..]).await {
+                            Ok(..) => (),
+                            Err(error) => {
+                                println!("Warning: failed to read exact frame: {error:?}");
+                                // TODO: deduplicate
+                                if let Some(raw_err) = error.raw_os_error() {
+                                    if raw_err == 22 {
+                                        continue 'connect_loop;
+                                    }
+                                }
+                                // continue 'init;
+                            }
+                        };
+
+                        let mut decoder = image::io::Reader::new(Cursor::new(buf.clone()));
+                        decoder.set_format(image::ImageFormat::Jpeg);
+
+                        let image = decoder.decode();
+
+                        if image.is_err() {
+                            println!("Warning: failed to decode image: {:?}", image.unwrap_err());
+                            continue;
                         }
+
+                        let image = image.unwrap().as_rgb8().unwrap().to_owned();
+
+                        let new_frame = Frame {
+                            timestamp: SystemTime::now(),
+                            raw_data: buf,
+                            decoded: image,
+                        };
+
+                        let _ = sender.broadcast_direct(new_frame).await;
                     }
+
+                    // println!("{:?} frame! {}", eye, port.bytes_to_read().unwrap());
                 }
-
-                loop {
-                    let mut buf = [0u8; 6];
-
-                    let to_copy = std::cmp::min(remaining_bytes.len(), 6);
-                    buf[..to_copy].copy_from_slice(&remaining_bytes[..to_copy]);
-                    remaining_bytes.drain(0..to_copy);
-                    match port.read_exact(&mut buf[to_copy..]).await {
-                        Ok(..) => (),
-                        Err(error) => {
-                            println!("Warning: failed to read exact frame: {}", error.kind());
-                            // continue 'init;
-                        }
-                    };
-
-                    if buf[0..4] != ETVR_PACKET_HEADER {
-                        println!("Wrong packet header");
-                        continue 'init;
-                    }
-                    let packet_len = u16::from_le_bytes([buf[4], buf[5]]) as usize;
-
-                    let mut buf = vec![0; packet_len];
-
-                    let to_copy = std::cmp::min(remaining_bytes.len(), packet_len);
-                    buf[..to_copy].copy_from_slice(&remaining_bytes[..to_copy]);
-                    remaining_bytes.drain(0..to_copy);
-                    match port.read_exact(&mut buf[to_copy..]).await {
-                        Ok(..) => (),
-                        Err(error) => {
-                            println!("Warning: failed to read exact frame: {}", error.kind());
-                            // continue 'init;
-                        }
-                    };
-
-                    let mut decoder = image::io::Reader::new(Cursor::new(buf.clone()));
-                    decoder.set_format(image::ImageFormat::Jpeg);
-
-                    let image = decoder.decode();
-
-                    if image.is_err() {
-                        println!("Warning: failed to decode image");
-                        continue;
-                    }
-
-                    let image = image.unwrap().as_rgb8().unwrap().to_owned();
-
-                    let new_frame = Frame {
-                        timestamp: SystemTime::now(),
-                        raw_data: buf,
-                        decoded: image,
-                    };
-
-                    let _ = sender.broadcast_direct(new_frame).await;
-                }
-
-                // println!("{:?} frame! {}", eye, port.bytes_to_read().unwrap());
             }
         };
 
